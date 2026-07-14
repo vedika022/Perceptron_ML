57b91dd8db04574261bf20d2f704fb9eacb859da
vedika022 <gurav.vedika22@gmail.com>
2026-07-14 09:29:48 +0530

    Loading the iris data temporarily

--- Perceptron_Model.py
+++ Perceptron_Model.py
@@ -22,7 +22,7 @@ class Perceptron():
         return self
     
     def net_input(self, X) :
-        return np.dot(X, self.w[1:]) + self.w_[0]
+        return np.dot(X, self.w_[1:]) + self.w_[0]
 
     def predict(self, X) :
         return np.where(self.net_input(X) >= 0.0 , 1, -1)
